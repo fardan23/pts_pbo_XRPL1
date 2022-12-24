8f9c051708ece302a95c1ac5159014be82c3e7b6
Muhamad Fardan <mprdan23@gmail.com>
2022-12-24 12:52:47 +0700

    menambah semua file

---
+++ src/praktek_pts_fardan/Praktek_pts_fardan.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek_pts_fardan;
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
+/*import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;*/
+/**
+ *
+ * @author Administrator
+ */
+public class Praktek_pts_fardan {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        /*String name, address;
+        name = "Muhamad Fardan";
+        address = "KP. Cibeureum";
+        System.out.println("Nama : "+name);
+        System.out.println("Alamat : "+address);*/
+    
+    class variabel
+    {
+            private int panjang;
+            private int lebar;
+        {
+            double luas;
+            variabel p = new variabel();
+            p.panjang = 20;
+            p.lebar = 10;
+            luas = p.panjang * p.lebar;
+            System.out.println("Luas persegi : " +luas);
+        }
+     }
+        
+        /*String nama, jurusan, kelas, sekolah;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama : ");
+        nama = input.nextLine();
+        
+        System.out.print("Jurusan : ");
+        nama = input.nextLine();
+        
+        System.out.print("Kelas : ");
+        nama = input.nextLine();
+        
+        System.out.print("Sekolah : ");
+        nama = input.nextLine();*/
+        
+        /*String nama = " ";
+        nama = JOptionPane.showInputDialog("Masukkan nama");
+        String msg = "Selamat Belajar " +nama;
+        JOptionPane.showMessageDialog(null,msg);*/
+        
+             /*String nama;
+        
+             InputStreamReader isr = new InputStreamReader(System.in);
+        
+             BufferedReader br = new BufferedReader(isr);
+                
+             System.out.print("Nama: ");
+             nama = br.readLine();*/
+             
+             /*String kategori =" ";
+             int nilai_UAS_PBO;
+             
+             nilai_UAS_PBO = 79;
+             
+             if(nilai_UAS_PBO > 85)
+             {
+                 kategori = "Anda Lulus";
+             }
+             else
+             {
+                 kategori = "Anda tidak lulus";
+             }
+             
+             System.out.println(kategori);*/
+             
+             /*int pilihan;
+             pilihan = 3;
+             
+             switch (pilihan)
+             {
+                 case 1:
+                     System.out.println("Universitas Sumatera Utara");
+                     break;
+                 case 2:
+                     System.out.println("Universitas Negeri Medan");
+                     break;
+                 case 3:
+                     System.out.println("Universitas Islam Negeri Sumatera Utara");
+                     break;
+                     
+                 default:
+                 System.out.println("Universitas Medan Area");*/
+                 
+                 /*for(int a=1; a<=20; a++)
+                 {
+                     System.out.println(a);
+                 }*/
+                 
+                 /*int a;
+                 a = 1;
+                 while(a <= 15)
+                 {
+                     System.out.println(a);
+                     a++;
+                 }*/
+                 
+                 /*int a;
+                 a=1;
+                 
+                 do
+                 {
+                     System.out.println(a);
+                     a++;
+                 }
+                 
+                 while(a<=10);*/
+                 
+                 /*for(int a=1; a<=10; a++)
+                 {
+                     System.out.println("Selamat Belajar Bahasa Pemrograman Java OOP");
+                     
+                     a = a+1;
+                     if(a == 5)
+                         break;
+                 }
+                 
+                 System.out.println("Proses berhenti saat perlangan ke-5");*/
+                 
+                 /*int java[];
+                 java = new int[5];
+                 
+                 int i;
+                 
+                 java[0] = 2;
+                 java[1] = 1;
+                 java[2] = 4;
+                 java[3] = 10;
+                 java[4] = 3;
+                 
+                 for(i=0; i<java.length;i++)
+                 {
+                     System.out.println("java[+1+]="+java[i]);
+                 }*/
+                 
+                 /*Scanner inputan= new Scanner (System.in);
+                int A[][]=new int [2][2];
+                int B[][]=new int [2][2];
+                int C[][]=new int [2][2];
+                
+                System.out.println("Masukkan Nilai Matriks X");
+                System.out.println("=======================");
+                for(int i=0;i<2;i++)
+                {
+                    for(int j=0;j<2;j++)
+                    {
+                        System.out.print("[" +(i+1)+ "][" +(j+1)+ "]:");
+                        A[i][j]=inputan.nextInt();
+                    }
+                }
+                System.out.println("\nMasukkan Nilai Matriks Y");
+                System.out.println("=========================");
+                for(int i=0;i<2;i++)
+                {
+                    for(int j=0;j<2;j++)
+                    {
+                        System.out.print("[" +(i+1)+ "][" +(j+1)+ "]:");
+                        B[i][j]=inputan.nextInt();
+                    }
+                }*/
+                
+                /* Melakukan Penjumlahan Matriks*/
+                /*for(int i=0;i<2;i++)
+                {
+                    for(int j=0;j<2;j++)
+                    {
+                        C[i][j]=A[i][j]+B[i][j];
+                    }
+                }
+                System.out.println("\nHasil Penjumlahan Matriks ");
+                System.out.println("============================");
+                for(int i=0;i<2;i++)
+                {
+                    for(int j=0;j<2;j++)
+                    {
+                        System.out.print(+(C[i][j])+" ");
+                    }
+                    System.out.println(" ");
+                }*/
+                 
+        // TODO code application logic here
+    }
+}
+    
